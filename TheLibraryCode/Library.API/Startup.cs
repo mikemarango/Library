41c12cdb0b14bdb33218b69585772a10f846a252
Michael Marango <mike.marango@gmail.com>
2017-11-28 06:16:04 -0800

    no need in asp.net core 2

--- TheLibraryCode/Library.API/Startup.cs
+++ TheLibraryCode/Library.API/Startup.cs
@@ -34,7 +34,7 @@ namespace Library.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
